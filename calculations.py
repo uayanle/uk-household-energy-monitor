4a13b84f1d6ec1c6840fdc7adbc21e0eeb9c0681
Umulqayr Ayanle <uayanle@gmail.com>
2026-07-27 16:25:52 +0100

    upadted calculation file

--- calculations.py
+++ calculations.py
@@ -1,6 +1,3 @@
-import pandas as pa
-import matplotlib as mp
-import tkinter as tk
 
 
 def calculate_energy(power_watts, hours_used):
@@ -24,10 +21,3 @@ def calculate_annual_cost(daily_cost):
 
 def calculate_emissions(annual_energy_kwh, carbon_factor):
     return annual_energy_kwh * carbon_factor
-
-
-print(calculate_energy(2000, 2))
-print(calculate_daily_cost(4, 0.30))
-print(calculate_monthly_cost(1.20))
-print(calculate_annual_cost(1.20))
-print(calculate_emissions(1460, 0.18))

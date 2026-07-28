54d6a080f4c91117438529b14e5f95ce853c62f5
Umulqayr Ayanle <uayanle@gmail.com>
2026-07-28 17:39:23 +0100

    Updated mianpy

--- main.py
+++ main.py
@@ -28,25 +28,25 @@ hours_used = float(input('Enter hours used per day: '))
 # calculating daily energy
 
 daily_energy = calculate_energy(power_watts, hours_used)
-print(f'The energy (kWh): {daily_energy}')
+print(f'The energy (kWh): {daily_energy:.2f}')
 
 # calculating daily cost
 
 daily_cost = calculate_daily_cost(daily_energy, price_per_kwh)
-print(f'Daily cost (£): {daily_cost}')
+print(f'Daily cost (£): {daily_cost:.2f}')
 
 # calculating monthly cost
 
 monthly_cost = calculate_monthly_cost(daily_cost)
-print(f'Monthly cost (£): {monthly_cost}')
+print(f'Monthly cost (£): {monthly_cost:.2f}')
 
 # calculating annual cost
 
 annual_cost = calculate_annual_cost(daily_cost)
-print(f'Annual cost (£): {annual_cost}')
+print(f'Annual cost (£): {annual_cost:.2f}')
 
 # calculate emissions
 
 annual_energy = calculate_energy(power_watts, hours_used * 365)
 emissions = calculate_emissions(annual_energy, carbon_factor)
-print(f'Emissions: {emissions}')
+print(f'Emissions: {emissions:.2f}')

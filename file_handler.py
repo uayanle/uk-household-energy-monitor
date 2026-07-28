f6f23200fc28f3c72550ff8ca1f3c311711bbfb7
Umulqayr Ayanle <uayanle@gmail.com>
2026-07-28 16:51:55 +0100

    updated file handler

--- file_handler.py
+++ file_handler.py
@@ -1,32 +1,18 @@
 import pandas as pd
 
-class tariffs_df(pd.DataFrame):
-    """A pandas DataFrame subclass for tariff data."""
+def load_carbon_factor():
+    """Load the carbon factor from the CSV file and return the kg CO₂ per kWh."""
 
-    @property
-    def _constructor(self):
-        return tariffs_df
+    carbon_df = pd.read_csv('data/carbon_factors.csv')
 
-    @property
-    def _constructor_sliced(self):
-        return pd.Series
+    carbon_factor = carbon_df.loc[0, 'kgco2_per_kWh']
+    return carbon_factor
 
-    @classmethod
-    def from_csv(cls, file_path='tariffs', **kwargs):
-        df = pd.read_csv(file_path, **kwargs)
-        return cls(df)
+def load_tariff():
+    """Load the electricity tariff from the csv file then return the price per kwh"""
 
-    def get_tariff(self, tariff_name, column_name='TARIFF'):
-        if column_name not in self.columns:
-            raise KeyError(f"No '{column_name}' column in data.")
-        return self.loc[self[column_name] == tariff_name]
+    tariffs_df = pd.read_csv('data/tariffs.csv')
+    price = tariffs_df.loc[0, 'Price_per_kWh'] # extracts the value
 
-    def average_rate(self, rate_column='RATE'):
-        if rate_column not in self.columns:
-            raise KeyError(f"No '{rate_column}' column in data.")
-        return self[rate_column].astype(float).mean()
+    return price
 
-# read the csv
-
-tariffs_df = tariffs_df.from_csv('tariffs')
-print(tariffs_df)
